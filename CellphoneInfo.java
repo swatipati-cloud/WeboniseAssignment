eca76186a845e0fc1e01c15a65c9f6752c1b1037
swatipati-cloud <55143985+swatipati-cloud@users.noreply.github.com>
2019-09-11 14:04:11 +0530

    Update CellphoneInfo.java

--- CellphoneInfo.java
+++ CellphoneInfo.java
@@ -1,5 +1,19 @@
 import java.util.*;
 
+class DelayProvision{
+	//To provide the delay or interval for the user convinence.
+	public void doDelay(int ms){
+		try
+		{
+			Thread.sleep(ms);
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+}
+
+//Parent of all cellphones with common features and properties.
 class Cellphone{
 	protected String plateform;
 	protected String display;
@@ -9,50 +23,31 @@ class Cellphone{
 	protected String performance;
 	protected String model;
 	private Scanner scan = new Scanner(System.in);
+	protected DelayProvision delay = new DelayProvision();
 
 	public void doSms(){
 		System.out.println("Enter your sms : ");
 		String sms = scan.nextLine();
 		System.out.println("Sending your sms...");
-		try
-		{
-			Thread.sleep(2000);
-		}
-		catch(Exception e){}
+		delay.doDelay(2000);
 		System.out.println("sms sent.");
 	}
 	public void doMms(){
 		System.out.println("browse the multimedia : ");
 		String mms = scan.nextLine();
 		System.out.println("Sending your mms...");
-		try
-		{
-			Thread.sleep(2000);
-		}
-		catch(Exception e){}
+		delay.doDelay(2000);
 		System.out.println("mms sent.");
 	}
 	public void doCall(){
 		System.out.println("Enter number you want to call : ");
 		String sms = scan.nextLine();
 		System.out.println("Calling...");
-		try
-		{
-			Thread.sleep(2000);
-		}
-		catch(Exception e){}
+		delay.doDelay(2000);
 		System.out.println("call connected..");
-		try
-		{
-			Thread.sleep(500);
-		}
-		catch(Exception e){}
+		delay.doDelay(500);
 		System.out.println("Please speak..");
-		try
-		{
-			Thread.sleep(2000);
-		}
-		catch(Exception e){}
+		delay.doDelay(2000);
 		System.out.println("call disconnected..");
 	}
 	public void displayCellphone(){
@@ -89,20 +84,12 @@ class Samsung extends Cellphone{
 	public void samsungPay(){
 		System.out.println("Enter the amount u want to pay : ");
 		int amount = scan.nextInt();
-		try
-		{
-			Thread.sleep(1000);
-		}
-		catch(Exception e){}
+		delay.doDelay(1000);
 		System.out.println(amount + " is deducted from your account .");
 	}
 	public void samsungGalary(){
 		System.out.println("Viewing photos...wait");
-		try
-		{
-			Thread.sleep(1000);
-		}
-		catch(Exception e){}
+		delay.doDelay(1000);
 		System.out.println("Photos viewed.");
 	}
 	public void displaySamsung(){
@@ -135,28 +122,16 @@ class Oppo extends Cellphone{
 	}
 	public void faceUnlock(){
 		System.out.println("Unlocking face plz wait..");
-		try
-		{
-			Thread.sleep(1000);
-		}
-		catch(Exception e){}
+		delay.doDelay(1000);
 		System.out.println("Done.");
 
 	}
 	public void GPSTrack(){
 		System.out.println("Plz turn on your location.");
-		try
-		{
-			Thread.sleep(1000);
-		}
-		catch(Exception e){}
+		delay.doDelay(1000);
 		System.out.println("Enter the destination : ");
 		String destination = scan.nextLine();
-		try
-		{
-			Thread.sleep(1000);
-		}
-		catch(Exception e){}
+		delay.doDelay(1000);
 		System.out.println("Route selected ...\nyour location ----------------- " + destination);
 	}
 	public void FMRadio(){
@@ -185,11 +160,7 @@ class Mi extends Cellphone{
 	}
 	public void faceRecognation(){
 		System.out.println("capturing your face : ");
-		try
-		{
-			Thread.sleep(1000);
-		}
-		catch(Exception e){}
+		delay.doDelay(1000);
 		System.out.println("Face recognised.");
 	}
 	public void displayMi(){
@@ -199,163 +170,158 @@ class Mi extends Cellphone{
 	}
 }
 
+//Contains the methods which will call respctive cellphones methods after selection of cellphone
+class CellphoneChoice{ 
+	private Scanner scan = new Scanner(System.in);
+	private DelayProvision delay = new DelayProvision();
+	
+	//To call the functionality of samsung's cellphone after operation selection
+	public void samsungInfo(){ 
+		char ch = 'y';
+		//For the repetitive selections of samsung's operation.			
+		do{ 
+			System.out.println("Samsung features : "); 
+			delay.doDelay(500);
+			System.out.println("1.Call\n2.SMS\n3.MMS\n4.Samsung Pay\n5.Samsung galary\n6.Display Samsung features.");
+			System.out.print("choose the operation you want to do : ");
+			int opn = scan.nextInt();
+			delay.doDelay(500);
+			Samsung samsung = new Samsung();
+			switch(opn){
+				case 1:
+					samsung.doCall();
+					break;
+				case 2:
+					samsung.doSms();
+					break;
+				case 3:
+					samsung.doMms();
+					break;
+				case 4:
+					samsung.samsungPay();
+					break;
+				case 5:
+					samsung.samsungGalary();
+					break;
+				case 6:
+					samsung.displaySamsung();
+					break;
+				default : 
+					System.out.println("Operation not available .");
+			}		
+			delay.doDelay(500);
+			System.out.println("Do you want to use another operation of samsung : (y/n) ");
+			ch = scan.next().charAt(0);
+		}
+		while(ch=='y');
+	}
+	//To call the functionality of oppo's cellphone after operation selection  
+	public void oppoInfo(){	
+		char ch = 'y';
+		//For the repetitive selections of oppo's operation.
+		do{ 
+			System.out.println("Oppo features :- "); 
+			delay.doDelay(500);		
+			System.out.println("1.Call\n2.SMS\n3.MMS\n4.Face Unlock\n5.GPS Track\n6.FM Radio\n7.Display Oppo features.");
+			System.out.print("choose the operation you want to do : ");
+			int opn = scan.nextInt();
+			delay.doDelay(500);						
+			Oppo oppo = new Oppo();
+			switch(opn){
+				case 1:
+					oppo.doCall();
+					break;
+				case 2:
+					oppo.doSms();
+					break;
+				case 3:
+					oppo.doMms();
+					break;
+				case 4:
+					oppo.faceUnlock();
+					break;
+				case 5:
+					oppo.GPSTrack();
+					break;
+				case 6:
+					oppo.FMRadio();
+					break;
+				case 7:
+					oppo.displayOppo();
+					break;
+				default : 
+					System.out.println("Operation not available .");
+			}		
+			delay.doDelay(500);
+			System.out.println("Do you want to use another operation of oppo : (y/n) ");
+			ch = scan.next().charAt(0);
+		}
+		while(ch=='y');
+	}
+	//To call the functionality of mi's cellphone after operation selection
+	public void miInfo(){  
+		char ch = 'y';
+		//For the repetitive selections of mi's operation.
+		do{  
+			System.out.println("Mi features :- "); 
+			delay.doDelay(500);
+			System.out.println("1.Call\n2.SMS\n3.MMS\n4.Face Recognation\n5.Display MI features.");
+			System.out.print("choose the operation you want to do : ");
+			int opn = scan.nextInt();
+			delay.doDelay(500);	
+			Mi mi = new Mi();
+			switch(opn){
+				case 1:
+					mi.doCall();
+					break;
+				case 2:
+					mi.doSms();
+					break;
+				case 3:
+					mi.doMms();
+					break;
+				case 4:
+					mi.faceRecognation();
+					break;
+				case 5:
+					mi.displayMi();
+					break;
+				default : 
+					System.out.println("Operation not available .");
+			}		
+			delay.doDelay(500);
+			System.out.println("Do you want to use another operation of MI : (y/n) ");
+			ch = scan.next().charAt(0);
+		}
+		while(ch=='y');
+	}
+}
+
 public class CellphoneInfo{
 	public static void main(String args[]){
 		Scanner scan = new Scanner(System.in);
+		DelayProvision delay = new DelayProvision();
+		CellphoneChoice cellphone = new CellphoneChoice();
 		char c = 'y';
-		do{	
+		//For the repetitive selections of cellphone.	
+		do{ 
 			System.out.println("Following are available cellphones : ");
 			System.out.println("1.Samsung\n2.Oppo\n3.MI");
 			System.out.print("Enter your choice : ");
 			int choice = scan.nextInt();
-			try{
-				Thread.sleep(100);
-			}
-			catch(Exception e){}
+			delay.doDelay(100);
 			char ch = 'y';
 			switch(choice){
 				case 1:
-					ch = 'y';
-					do{			{	
-						System.out.println("Samsung features : "); 
-						try{
-							Thread.sleep(500);
-						}
-						catch(Exception e){}	
-						System.out.println("1.Call\n2.SMS\n3.MMS\n4.Samsung Pay\n5.Samsung galary\n6.Display Samsung features.");
-						System.out.print("choose the operation you want to do : ");
-						int opn = scan.nextInt();
-						try{
-							Thread.sleep(500);
-						}
-						catch(Exception e){}
-						Samsung samsung = new Samsung();
-						switch(opn){
-							case 1:
-								samsung.doCall();
-								break;
-							case 2:
-								samsung.doSms();
-								break;
-							case 3:
-								samsung.doMms();
-								break;
-							case 4:
-								samsung.samsungPay();
-								break;
-							case 5:
-								samsung.samsungGalary();
-								break;
-							case 6:
-								samsung.displaySamsung();
-								break;
-							default : 
-								System.out.println("Operation not available .");
-						}		
-						try{
-							Thread.sleep(500);
-						}
-						catch(Exception e){}
-						System.out.println("Do you want to use another operation of samsung : (y/n) ");
-						ch = scan.next().charAt(0);
-					}
-					while(ch=='y');
+					cellphone.samsungInfo();
 					break;
 			
 				case 2:
-					ch = 'y';
-					do{	
-						System.out.println("Oppo features :- "); 
-						try{				{
-							Thread.sleep(500);
-						}
-						catch(Exception e){}		
-						System.out.println("1.Call\n2.SMS\n3.MMS\n4.Face Unlock\n5.GPS Track\n6.FM Radio\n7.Display Oppo features.");
-						System.out.print("choose the operation you want to do : ");
-						int opn = scan.nextInt();
-						try{
-							Thread.sleep(500);
-						}
-						catch(Exception e){}
-						Oppo oppo = new Oppo();
-						switch(opn){
-							case 1:
-								oppo.doCall();
-								break;
-							case 2:
-								oppo.doSms();
-								break;
-							case 3:
-								oppo.doMms();
-								break;
-							case 4:
-								oppo.faceUnlock();
-								break;
-							case 5:
-								oppo.GPSTrack();
-								break;
-							case 6:
-								oppo.FMRadio();
-								break;
-							case 7:
-								oppo.displayOppo();
-								break;
-							default : 
-								System.out.println("Operation not available .");
-						}		
-						try{
-							Thread.sleep(500);
-						}
-						catch(Exception e){}
-						System.out.println("Do you want to use another operation of oppo : (y/n) ");
-						ch = scan.next().charAt(0);
-					}
-					while(ch=='y');
+					cellphone.oppoInfo();
 					break;
 			
 				case 3:
-					ch = 'y';
-					do{	
-						System.out.println("Mi features :- "); 
-						try{
-							Thread.sleep(500);
-						}
-						catch(Exception e){}		
-						System.out.println("1.Call\n2.SMS\n3.MMS\n4.Face Recognation\n5.Display MI features.");
-						System.out.print("choose the operation you want to do : ");
-						int opn = scan.nextInt();
-						try{
-							Thread.sleep(500);
-						}	
-						catch(Exceptione){}
-						Mi mi = new Mi();
-						switch(opn){
-							case 1:
-								mi.doCall();
-								break;
-							case 2:
-								mi.doSms();
-								break;
-							case 3:
-								mi.doMms();
-								break;
-							case 4:
-								mi.faceRecognation();
-								break;
-							case 5:
-								mi.displayMi();
-								break;
-							default : 
-								System.out.println("Operation not available .");
-						}		
-						try{
-							Thread.sleep(500);
-						}
-						catch(Exception e){}
-						System.out.println("Do you want to use another operation of MI : (y/n) ");
-						ch = scan.next().charAt(0);
-					}
-					while(ch=='y');
+					cellphone.miInfo();
 					break;
 				
 				default:
